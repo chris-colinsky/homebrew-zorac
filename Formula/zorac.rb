cf8b853c3b3be2e40e6eda2f096996c006bb9481
Chris Colinsky <88026078+chris-colinsky@users.noreply.github.com>
2026-02-26 18:40:35 -0800

    Refactor Zorac formula for installation and homepage
    Updated homepage URL and modified install method for Zorac.

--- Formula/zorac.rb
+++ Formula/zorac.rb
@@ -1,20 +1,31 @@
 class Zorac < Formula
-  include Language::Python::Virtualenv
-
   desc "Self-hosted local LLM chat client for vLLM inference servers"
-  homepage "https://github.com/chris-colinsky/zorac"
+  homepage "https://github.com/chris-colinsky/chris-colinsky/zorac"
   url "https://files.pythonhosted.org/packages/8d/05/512c4d0d270e615e73336b7acc2ea7afb5adb50a472065e376a1a7117b0b/zorac-1.4.0.tar.gz"
   sha256 "185a8c82ab9776d98cb821f93746765aaee2a5217b36534a83b6c3c968ec245d"
   license "MIT"
 
   depends_on "python@3.13"
 
-  def install
-    virtualenv_install_with_resources
+  skip_clean "libexec"
+
+  def fix_install_names(flags)
+    # Do nothing
   end
 
-  def relocate_binaries?
-    false
+  def install
+    python = Formula["python@3.13"].opt_bin/"python3.13"
+
+    system python, "-m", "venv", libexec
+    system libexec/"bin/pip", "install", "--upgrade", "pip"
+    
+    # We install the package normally. Pip handles the dependencies (like dotenv).
+    system libexec/"bin/pip", "install", "zorac==1.4.0"
+
+    (bin/"zorac").write <<~EOS
+      #!/bin/bash
+      exec "#{libexec}/bin/zorac" "$@"
+    EOS
   end
 
   test do
