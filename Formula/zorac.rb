ea8c4f07e5223989da30dd6cedf7a08cc22646e9
Chris Colinsky <88026078+chris-colinsky@users.noreply.github.com>
2026-02-26 18:42:09 -0800

    Fix homepage URL and update install comments
    Updated homepage URL and modified install method for clarity.

--- Formula/zorac.rb
+++ Formula/zorac.rb
@@ -1,6 +1,6 @@
 class Zorac < Formula
   desc "Self-hosted local LLM chat client for vLLM inference servers"
-  homepage "https://github.com/chris-colinsky/chris-colinsky/zorac"
+  homepage "https://github.com/chris-colinsky/zorac"
   url "https://files.pythonhosted.org/packages/8d/05/512c4d0d270e615e73336b7acc2ea7afb5adb50a472065e376a1a7117b0b/zorac-1.4.0.tar.gz"
   sha256 "185a8c82ab9776d98cb821f93746765aaee2a5217b36534a83b6c3c968ec245d"
   license "MIT"
@@ -9,8 +9,8 @@ class Zorac < Formula
 
   skip_clean "libexec"
 
-  def fix_install_names(flags)
-    # Do nothing
+  def relocatable?
+    false
   end
 
   def install
@@ -19,7 +19,7 @@ class Zorac < Formula
     system python, "-m", "venv", libexec
     system libexec/"bin/pip", "install", "--upgrade", "pip"
     
-    # We install the package normally. Pip handles the dependencies (like dotenv).
+    # We install normally; pip handles the heavy lifting
     system libexec/"bin/pip", "install", "zorac==1.4.0"
 
     (bin/"zorac").write <<~EOS
